d580a4d64bdfa12752fbcf0e2c9ae2f6d7c377a9
haijima <k.haijima@gmail.com>
2024-01-01 22:24:49 +0900

    skeleton init

---
+++ internal/tablecheck_test.go
@@ -0,0 +1,15 @@
+package internal_test
+
+import (
+	"testing"
+
+	"github.com/gostaticanalysis/testutil"
+	"github.com/haijima/scone/internal"
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+// TestAnalyzer is a test for Analyzer.
+func TestAnalyzer(t *testing.T) {
+	testdata := testutil.WithModules(t, analysistest.TestData(), nil)
+	analysistest.Run(t, testdata, internal.Analyzer, "a")
+}

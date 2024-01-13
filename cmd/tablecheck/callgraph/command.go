907aa9a72dbb62f288b73a1c263926bacb9fa860
haijima <k.haijima@gmail.com>
2024-01-13 10:55:41 +0900

    feat: Add some flags to `callgraph` command

--- cmd/tablecheck/callgraph/command.go
+++ cmd/tablecheck/callgraph/command.go
@@ -25,6 +25,20 @@ func NewCommand(v *viper.Viper, _ afero.Fs) *cobra.Command {
 	cmd.Flags().StringP("pattern", "p", "./...", "The pattern to analyze")
 	cmd.Flags().String("format", "dot", "The output format {dot|mermaid|text}")
 	cmd.Flags().StringSlice("exclude-queries", []string{}, "The `SHA1s` of queries to exclude")
+	cmd.Flags().StringSlice("exclude-packages", []string{}, "The `names` of packages to exclude")
+	cmd.Flags().StringSlice("exclude-package-paths", []string{}, "The `paths` of packages to exclude")
+	cmd.Flags().StringSlice("exclude-files", []string{}, "The `names` of files to exclude")
+	cmd.Flags().StringSlice("exclude-functions", []string{}, "The `names` of functions to exclude")
+	cmd.Flags().StringSlice("exclude-query-types", []string{}, "The `types` of queries to exclude {select|insert|update|delete}")
+	cmd.Flags().StringSlice("exclude-tables", []string{}, "The `names` of tables to exclude")
+	cmd.Flags().StringSlice("filter-queries", []string{}, "The `SHA1s` of queries to filter")
+	cmd.Flags().StringSlice("filter-packages", []string{}, "The `names` of packages to filter")
+	cmd.Flags().StringSlice("filter-package-paths", []string{}, "The `paths` of packages to filter")
+	cmd.Flags().StringSlice("filter-files", []string{}, "The `names` of files to filter")
+	cmd.Flags().StringSlice("filter-functions", []string{}, "The `names` of functions to filter")
+	cmd.Flags().StringSlice("filter-query-types", []string{}, "The `types` of queries to filter {select|insert|update|delete}")
+	cmd.Flags().StringSlice("filter-tables", []string{}, "The `names` of tables to filter")
+	cmd.Flags().String("mode", "ssa-method", "The query analyze `mode` {ssa-method|ssa-const|ast}")
 	_ = cmd.MarkFlagDirname("dir")
 
 	return cmd
@@ -34,8 +48,50 @@ func run(cmd *cobra.Command, v *viper.Viper) error {
 	dir := v.GetString("dir")
 	pattern := v.GetString("pattern")
 	excludeQueries := v.GetStringSlice("exclude-queries")
+	excludePackages := v.GetStringSlice("exclude-packages")
+	excludePackagePaths := v.GetStringSlice("exclude-package-paths")
+	excludeFiles := v.GetStringSlice("exclude-files")
+	excludeFunctions := v.GetStringSlice("exclude-functions")
+	excludeQueryTypes := v.GetStringSlice("exclude-query-types")
+	excludeTables := v.GetStringSlice("exclude-tables")
+	filterQueries := v.GetStringSlice("filter-queries")
+	filterPackages := v.GetStringSlice("filter-packages")
+	filterPackagePaths := v.GetStringSlice("filter-package-paths")
+	filterFiles := v.GetStringSlice("filter-files")
+	filterFunctions := v.GetStringSlice("filter-functions")
+	filterQueryTypes := v.GetStringSlice("filter-query-types")
+	filterTables := v.GetStringSlice("filter-tables")
+	modeFlg := v.GetString("mode")
 
-	result, err := tablecheck.Analyze(dir, pattern, &query.Option{ExcludeQueries: excludeQueries})
+	var mode query.AnalyzeMode
+	if modeFlg == "ssa-method" {
+		mode = query.SsaMethod
+	} else if modeFlg == "ssa-const" {
+		mode = query.SsaConst
+	} else if modeFlg == "ast" {
+		mode = query.Ast
+	} else {
+		return fmt.Errorf("unknown mode: %s", modeFlg)
+	}
+
+	opt := &query.Option{
+		Mode:                mode,
+		ExcludeQueries:      excludeQueries,
+		ExcludePackages:     excludePackages,
+		ExcludePackagePaths: excludePackagePaths,
+		ExcludeFiles:        excludeFiles,
+		ExcludeFunctions:    excludeFunctions,
+		ExcludeQueryTypes:   excludeQueryTypes,
+		ExcludeTables:       excludeTables,
+		FilterQueries:       filterQueries,
+		FilterPackages:      filterPackages,
+		FilterPackagePaths:  filterPackagePaths,
+		FilterFiles:         filterFiles,
+		FilterFunctions:     filterFunctions,
+		FilterQueryTypes:    filterQueryTypes,
+		FilterTables:        filterTables,
+	}
+	result, err := tablecheck.Analyze(dir, pattern, opt)
 	if err != nil {
 		return err
 	}

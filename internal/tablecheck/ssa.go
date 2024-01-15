fb0c59ac33b9b18a1f4131cc86c7d537cf546d3f
haijima <k.haijima@gmail.com>
2024-01-15 23:47:55 +0900

    chore: Change `ssa.BuilderMode`

--- internal/tablecheck/ssa.go
+++ internal/tablecheck/ssa.go
@@ -11,7 +11,7 @@ import (
 
 // See: buildssa.Analyzer
 func BuildSSA(pkg *packages.Package) (*buildssa.SSA, error) {
-	prog := ssa.NewProgram(pkg.Fset, ssa.GlobalDebug)
+	prog := ssa.NewProgram(pkg.Fset, ssa.BuilderMode(0))
 
 	// Create SSA packages for direct imports.
 	for _, p := range pkg.Types.Imports() {

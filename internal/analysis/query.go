e5c05dc97294097eac52c7ee5b594d2333dd754e
haijima <k.haijima@gmail.com>
2024-02-08 00:37:25 +0900

    fix: Remove redundant logs

--- internal/analysis/query.go
+++ internal/analysis/query.go
@@ -243,8 +243,9 @@ func constLikeStringValueToQueryGroup(pkg *ssa.Package, v ssa.Value, fn *ssa.Fun
 			if q, ok := sql.ParseString(a); ok {
 				if opt.Filter(q, meta) {
 					qr.Append(q)
+				} else {
+					slog.Debug("filtered out: sql", "SQL", q.String(), "package", pkg.Pkg.Path(), "file", analysisutil.FLC(meta.Position()))
 				}
-				slog.Debug("filtered out: sql", "SQL", q.String(), "package", pkg.Pkg.Path(), "file", analysisutil.FLC(meta.Position()))
 			} else {
 				if norm, err := sql.Normalize(a); err == nil {
 					a = norm

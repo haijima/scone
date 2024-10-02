1796d76617280d45b62b57327f3a0469175ecfa4
haijima <k.haijima@gmail.com>
2024-10-02 10:11:52 +0900

    test: Add a test for loop command

--- cmd/scone/loop_test.go
+++ cmd/scone/loop_test.go
@@ -6,9 +6,11 @@ import (
 	"io"
 	"testing"
 
+	"github.com/sebdah/goldie/v2"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_runLoop_singleProject(t *testing.T) {
@@ -25,4 +27,28 @@ func Test_runLoop_singleProject(t *testing.T) {
 
 	err := runLoop(cmd, v)
 	assert.NoError(t, err)
+}
+
+func Test_runLoop(t *testing.T) {
+	tests := []string{"isucon10-qualify", "isucon10-final", "isucon11-qualify", "isucon11-final", "isucon12-qualify", "isucon12-final", "isucon13"}
+	for _, tt := range tests {
+		t.Run(tt, func(t *testing.T) {
+			cmd := &cobra.Command{}
+			cmd.SetContext(context.Background())
+			buf := &bytes.Buffer{}
+			cmd.SetOut(buf)
+			cmd.SetErr(io.Discard)
+			v := viper.New()
+			v.Set("dir", "./testdata/src/"+tt)
+			v.Set("pattern", "./...")
+			v.Set("format", "table")
+			v.Set("analyze-funcs", []string{"github.com/isucon/isucon12-qualify/webapp/go.dbOrTx.GetContext@2", "github.com/isucon/isucon12-qualify/webapp/go.dbOrTx.SelectContext@2", "github.com/isucon/isucon12-qualify/webapp/go.dbOrTx.ExecContext@1"})
+
+			err := runLoop(cmd, v)
+			require.NoError(t, err)
+
+			g := goldie.New(t)
+			g.Assert(t, tt+".loop", buf.Bytes())
+		})
+	}
 }
